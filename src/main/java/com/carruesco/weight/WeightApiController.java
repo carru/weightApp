f73191a298cdd36f3a2d61f6c625ed94b63e1ced
carru <carru400@gmail.com>
2017-07-19 22:30:50 +0200

    Add APIs to add single weight samples

--- src/main/java/com/carruesco/weight/WeightApiController.java
+++ src/main/java/com/carruesco/weight/WeightApiController.java
@@ -1,10 +1,15 @@
 package com.carruesco.weight;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +18,8 @@ public class WeightApiController {
 	@Autowired
     JdbcTemplate jdbcTemplate;
 
+	private final String SUCCESS = "OK";
+	
     @GetMapping("/api/weights")
     public DataTablePojo weights() {
     	List<Weight> weights = new ArrayList<Weight>();
@@ -24,5 +31,39 @@ public class WeightApiController {
         ).forEach(weight -> weights.add(weight));
     	
     	return new DataTablePojo(weights);
+    }
+    
+//    @PostMapping("/api/weight")
+    @RequestMapping("/api/weight")
+    public String addWeight(@RequestParam(value="date") String date, 
+    		@RequestParam(value="weight") String weight, 
+    		@RequestParam(value="height") String height) {
+    	
+    	if ("".equals(height)) height = getLastHeight();
+    	String sql = "INSERT INTO weight.weights (date, weight, height)" + 
+    				 " VALUES (?, ?, ?)" +
+    				 " ON DUPLICATE KEY UPDATE weight = ?, height = ?;";
+    	Object[] args = new Object[] { date, weight, height, weight, height };
+    	
+    	try {
+    		jdbcTemplate.update(sql, args);    		
+    	} catch (Exception e) {
+			return e.getMessage();
+		}
+    	
+    	return SUCCESS;
+    }
+    
+//    @PostMapping("/api/weightOnly")
+    @RequestMapping("/api/weightOnly")
+    public String addWeight(@RequestParam(value="weight") String weight) {
+		String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    	String height = "";
+    	return addWeight(date, weight, height);
+    }
+    
+    private String getLastHeight() {
+    	String sql = "SELECT height FROM weight.weights ORDER BY date DESC LIMIT 1;";
+    	return jdbcTemplate.queryForObject(sql, String.class);
     }
 }
