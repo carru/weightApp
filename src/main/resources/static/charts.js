f82e30604ffe5267f903882e9f0629b3e7ff97e8
carru <carru400@gmail.com>
2017-07-21 20:44:27 +0200

    Dynamic horizontal gridlines

--- src/main/resources/static/charts.js
+++ src/main/resources/static/charts.js
@@ -2,16 +2,28 @@ google.charts.load('current', {'packages':['corechart', 'controls']});
 google.charts.setOnLoadCallback(setupAndDraw);
 
 var dashboard, data;
-
 var series = {
-	0 : { color: '#000000', type: 'line' }, // weight
-	1 : { color: '#ff9999', type: 'area' }, // bmi 18.5
-	2 : { color: '#99ff99', type: 'area' }, // bmi 25
-	3 : { color: '#ffcc00', type: 'area' }, // bmi 30
-	4 : { color: '#cc0000', type: 'area' }  // bmi 35
+	0: { color: '#000000', type: 'line' }, // weight
+	1: { color: '#ff9999', type: 'area', lineWidth: 0 }, // bmi 18.5
+	2: { color: '#99ff99', type: 'area', lineWidth: 0 }, // bmi 25
+	3: { color: '#ffcc00', type: 'area', lineWidth: 0 }, // bmi 30
+	4: { color: '#cc0000', type: 'area', lineWidth: 0 }  // bmi 35
 };
+var aMax = 100;
+var aMin = 70;
+var tickStep = 5;
 
 $(window).resize(function() { redraw(); });
+
+function getTickValues(min, max, step) {
+	var ticks = [ min ];
+	var currentTick = min + step;
+	while (currentTick <= max) {
+	    ticks.push(currentTick);
+	    currentTick = currentTick + step;
+	}
+	return ticks;
+}
 
 function setupAndDraw() {
 	var jsonData = $.ajax({
@@ -31,7 +43,12 @@ function setupAndDraw() {
 			series: series,
 			theme: 'maximized',
             isStacked: true,
-            vAxis: { viewWindow: { min: 70, max: 100 } }
+            vAxis: { 
+            	viewWindow: { min: aMin, max: aMax },
+            	ticks: getTickValues(aMin, aMax, tickStep),
+            	minorGridlines: { count: tickStep - 1 }
+            },
+            trendlines: { 0: {type: 'polynomial', degree: 4, color: '#999999', opacity: 0.5} }
 		}
 	});
 
@@ -47,7 +64,7 @@ function setupAndDraw() {
 					series: series,
                     chartArea: { width: '95%' },
                     isStacked: true,
-                    vAxis: { viewWindow: { min: 70, max: 100 } }
+                    vAxis: { viewWindow: { min: aMin, max: aMax } }
 				}
 			}
 		},
